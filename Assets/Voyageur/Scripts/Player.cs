62b6d9e5c416ddd3a8075d81ad2badbc0a62079b
Jamie <jmb1g17@soton.ac.uk>
2020-02-04 10:44:28 +0000

    Initial Changes
    I reworked some of the file structure and set up the URP properly as I hadn't done before. I also created a skeleton of the Player script of what would need to go in there

---
+++ Assets/Voyageur/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	[Header("Player Stats")]
+	public float stamina;
+	public float speed;
+
+	[Header("Player Inventory")]
+	public bool hasWood;
+	public bool hasStick;
+	public bool hasFish;
+	public bool hasCanoe;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	//Controls the player movment when not holding the canoe
+	void Move()
+	{
+
+	}
+	//Detects input and what interacted with
+	void Interact()
+	{
+
+	}
+	//Handles the picking up/placing down the canoe
+	void HandleCanoe()
+	{
+
+	}
+
+}
