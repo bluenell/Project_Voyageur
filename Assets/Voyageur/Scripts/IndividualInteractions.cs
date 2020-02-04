f42f75986fbe0249fa028095f6cfbe4f042cd824
Jamie <jmb1g17@soton.ac.uk>
2020-02-04 11:18:48 +0000

    Create Static Voids
    Created a static void for each input interaction and added comments for what each interaction will do

---
+++ Assets/Voyageur/Scripts/IndividualInteractions.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndividualInteractions : Interactions
+{
+	
+
+	static void Sleep()
+	{
+		//check time
+		//play animations
+		//play sounds
+		//advance time to 06:00
+	}
+
+	static void Eat()
+	{
+		 //check fish
+		 //play animation
+		 //stamina = max
+	}
+
+	static void Harmonica()
+	{
+		//play clip at index
+		//advance index
+		//play animation
+		//exit
+
+
+	}
+
+	static void PetDog()
+	{
+		//play animation
+		//play sound
+	}
+
+	static void Chop()
+	{
+		//play animation
+		//play sound
+		//increase counter
+		//if counter = max, destroy object
+
+	}
+
+	static void Collect()
+	{
+		//play animation
+		//play sound
+		//increase counter
+		//if counter = max, destroy object
+		//add to inventory
+
+	}
+
+	static void LightFire()
+	{
+		//check if wood
+		//play animation
+		//play sound
+		//create fire object
+		
+	}
+
+	static void InvestigateMine()
+	{
+		//play animation
+	}
+}
