bd15bc9a602eb2762357678ca88b909410d2ef34
Jamie <jmb1g17@soton.ac.uk>
2020-02-04 15:15:17 +0000

    Basic player movement
    Added basic player movement and colliders for the first pathway so the player can't leave the path. Also imported the walk animation

--- Assets/Voyageur/Scripts/Player.cs
+++ Assets/Voyageur/Scripts/Player.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+	
 
 	[Header("Player Stats")]
 	public float stamina;
@@ -14,23 +16,61 @@ public class Player : MonoBehaviour
 	public bool hasStick;
 	public bool hasFish;
 	public bool hasCanoe;
-
+	
+	
+	Rigidbody2D rb;
+	SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+		rb = GetComponent<Rigidbody2D>();
+		sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+		Move();
     }
 
 	//Controls the player movment when not holding the canoe
 	void Move()
 	{
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		rb.velocity = new Vector2(moveX, moveY) * speed;
+
+		float yPos = transform.position.y;
+		float yScaleFactor = 10;
+		float scaleMax = 0.9f;
+
+		
+		
+
+
+
+		#region FlipCharacter
+		if (moveX < 0f)
+		{
+			sprite.flipX = true;
+		}
+		if (moveX > 0f)
+		{
+		 sprite.flipX = false;
+		}
+
+
+
+
+		#endregion
+
+
+
+
+		
+
 
 	}
 	//Detects input and what interacted with
