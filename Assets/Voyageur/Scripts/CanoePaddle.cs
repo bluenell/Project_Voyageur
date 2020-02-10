d084dd9600f70d36ff360f6090183e02de2994a4
Jamie <jmb1g17@soton.ac.uk>
2020-02-10 13:56:05 +0000

    Added canoe movement physics
    Canoe has a deceleration rate that is applied when the player lets go of the input

--- Assets/Voyageur/Scripts/CanoePaddle.cs
+++ Assets/Voyageur/Scripts/CanoePaddle.cs
@@ -10,29 +10,60 @@ public class CanoePaddle : MonoBehaviour
 
 	public float paddleForce;
 
+	Vector2 movement;
+	Vector2 momementum = new Vector2();
+	public float acceleration;
+	public float friction;
+	public float stopValue;
+	public float maxSpeed;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
 	{
-		if (Input.GetAxis("Horizontal") > 0)
+
+		float xDir = Input.GetAxis("Horizontal");
+		movement = new Vector2(xDir, 0);
+
+		momementum -= (momementum.normalized * acceleration * Time.deltaTime * friction);
+
+
+
+		if (xDir <= 0)
 		{
-			Paddle();
+			if (momementum.magnitude < stopValue)
+			{
+
+				momementum = new Vector2();
+				Float();
+			}
 		}
-		else
+
+		momementum += movement;
+
+
+		if (momementum.magnitude > (maxSpeed * Time.deltaTime)) 
 		{
-			Float();
+			momementum = momementum.normalized * (maxSpeed * Time.deltaTime);
 		}
-    }
+
+		transform.Translate(momementum);
+
+	}
 
 
 	void Paddle()
 	{
-		rb.velocity = new Vector2(paddleForce, 0);
+
 	}
 
 
