82654f70efd2290b9188bc3c890948a6265a8898
Jamie <jmb1g17@soton.ac.uk>
2020-02-10 11:02:08 +0000

    Added canoe object
    Added the canoe object and attached collider, rigidbody and an empty CS script for the movement

--- Assets/Voyageur/Scripts/CanoePaddle.cs
+++ Assets/Voyageur/Scripts/CanoePaddle.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CanoePaddle : MonoBehaviour
 {
+
+	Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
