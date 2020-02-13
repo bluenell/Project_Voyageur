f5d4315e521caf676988d62ee10c237557461436
Jamie <jmb1g17@soton.ac.uk>
2020-02-13 20:04:19 +0000

    Button press
    canoe workls on button press and adds a burst of momentum to the object.

---
+++ Assets/Voyageur/Scripts/CanoePaddleV2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanoePaddleV2 : MonoBehaviour
+{
+
+	bool canPaddle = true;
+
+	float counter, x;
+	public int countMax;
+
+	public float maxSpeed,decelerationRate, friction;
+
+
+	Vector2 movement, momentum;
+
+
+    void Update()
+    {
+		x = Input.GetAxis("Horizontal");
+
+		if (x > 0)
+		{
+			Paddle();
+			
+		}
+
+    }
+
+
+
+	void Paddle()
+	{
+
+		movement = new Vector2(x, 0);
+
+		momentum += movement;
+
+
+		if (x <= 0)
+		{
+			momentum -= ((momentum.normalized * decelerationRate * Time.deltaTime * friction));
+		}
+
+		if (momentum.magnitude > (maxSpeed * Time.deltaTime))
+		{
+			momentum = momentum.normalized * (maxSpeed * Time.deltaTime);
+		}
+
+		transform.Translate(momentum);
+
+	}
+}
