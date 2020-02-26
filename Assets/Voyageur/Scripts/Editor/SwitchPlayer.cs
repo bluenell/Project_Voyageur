cffbe998e47c35ace0929dba1bb0909aea7c063c
Jamie <jmb1g17@soton.ac.uk>
2020-02-26 15:24:01 +0000

    Started Editor Script
    Started an editor script to switch between the player and the canoe

--- Assets/Voyageur/Scripts/Editor/SwitchPlayer.cs
+++ Assets/Voyageur/Scripts/Editor/SwitchPlayer.cs
@@ -3,8 +3,54 @@ using UnityEditor;
 
 public class SwitchPlayer : EditorWindow
 {
-	private void OnGUI()
+	
+	public static GameObject player;
+	public static GameObject canoe;
+	public static GameObject canoeAIO;
+	public static GameObject monty;
+	public static GameObject layerManager;
+	public static GameObject interactionsManager;
+
+	public static GameObject canoeCam;
+	public static GameObject playerCam;
+
+
+	private void OnEnable()
 	{
+		player = ((GameObject)GameObject.Find("Player"));
+		canoe = ((GameObject)GameObject.Find("CanoeSingle"));
+		canoeAIO = ((GameObject)GameObject.Find("Canoe AIO"));
+		monty = ((GameObject)GameObject.Find("Monty"));
+		layerManager = ((GameObject)GameObject.Find("LayerManager"));
+		interactionsManager = ((GameObject)GameObject.Find("InteractionsManager"));
+		canoeCam = ((GameObject)GameObject.Find("CanoeCam"));
+		playerCam = ((GameObject)GameObject.Find("PlayerCam"));
+
+	}
+
+	[MenuItem("Testing/Switch to Player")]
+	static void SwitchToPlayer()
+	{
+		canoeAIO.SetActive(false);
+		canoe.SetActive(true);
+		player.SetActive(true);
+		monty.SetActive(true);
+		layerManager.SetActive(true);
+		interactionsManager.SetActive(true);
+		canoeCam.SetActive(false);
+		playerCam.SetActive(true);
+	}
+	[MenuItem("Testing/Switch to Canoe")]
+	static void SwitchToCanoe()
+	{
+		canoeAIO.SetActive(true);
+		canoe.SetActive(false);
+		player.SetActive(false);
+		monty.SetActive(false);
+		layerManager.SetActive(false);
+		interactionsManager.SetActive(false);
+		canoeCam.SetActive(true);
+		playerCam.SetActive(false);
 
 	}
 
