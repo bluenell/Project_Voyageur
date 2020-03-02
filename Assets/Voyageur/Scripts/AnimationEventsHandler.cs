19bfba4d017c1ca763935b82216f37511c0a0fd2
Jamie <jmb1g17@soton.ac.uk>
2020-03-02 14:32:06 +0000

    received

--- Assets/Voyageur/Scripts/AnimationEventsHandler.cs
+++ Assets/Voyageur/Scripts/AnimationEventsHandler.cs
@@ -7,10 +7,14 @@ public class AnimationEventsHandler : MonoBehaviour
 	TransitionHandler TransitionHandler;
 	PlayerSoundManager playerSoundManager;
 
+	GameObject player;
+
 
 	private void Awake()
 	{
+		player = GameObject.Find("Player");
 		TransitionHandler = GameObject.Find("Transition Handler").GetComponent<TransitionHandler>();
+
 		
 	}
 	public void Transition()
@@ -26,7 +30,7 @@ public class AnimationEventsHandler : MonoBehaviour
 
 	public void FootStepsPlayer()
 	{
-		playerSoundManager.PlayFootsteps();
+		player.GetComponent<PlayerSoundManager>().PlayFootsteps();
 	}
 
 }
