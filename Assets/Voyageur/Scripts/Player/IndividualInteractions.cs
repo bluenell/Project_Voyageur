8bd244915a22bec5abf0483071487affd3d5e44b
Jamie <jmb1g17@soton.ac.uk>
2020-03-06 13:57:12 +0000

    added audio to bluejay
    set min sound range to the trigger box, and make the max sound very big

--- Assets/Voyageur/Scripts/Player/IndividualInteractions.cs
+++ Assets/Voyageur/Scripts/Player/IndividualInteractions.cs
@@ -163,6 +163,7 @@ public class IndividualInteractions : MonoBehaviour
 		anim = manager.interaction.gameObject.transform.GetChild(0).GetComponent<Animator>();
 
 		anim.SetTrigger("Play");
+		manager.interaction.gameObject.transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
 	}
 
 
