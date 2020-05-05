7148a830a04ae4ee29906aa8979d246b3b732997
Jamie <jmb1g17@soton.ac.uk>
2020-05-05 15:28:40 +0100

    Island 1 Sprite Flickering Issues
    Create a gap of 5 between layers, made foreground layer larger

---
+++ Assets/Voyageur/Scripts/CanoeSortingLayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanoeSortingLayer : MonoBehaviour
+{
+	public SpriteRenderer playerSprite, canoeSprite;
+
+
+	public void UpdateSprite()
+	{
+		canoeSprite.sortingOrder = playerSprite.sortingOrder;
+		playerSprite.sortingOrder = canoeSprite.sortingOrder + 1;
+	}
+}
