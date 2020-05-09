ee3c92cec5e7ea8df42303dd6f4d0f7fd9779b01
Jamie <jmb1g17@soton.ac.uk>
2020-05-09 11:29:20 +0100

    Fixed some major bugs

--- Assets/Voyageur/Scripts/Player/PlayerController.cs
+++ Assets/Voyageur/Scripts/Player/PlayerController.cs
@@ -270,17 +270,6 @@ public class PlayerController : MonoBehaviour
 					{
 						usingAxe = true;
 
-
-						if (interactionsManager.interaction.forceFaceRight)
-						{
-							sprite.flipX = false;
-						}
-						else
-						{
-							sprite.flipX = true;
-						}
-
-
 					}
 					if (currentInventoryIndex == 2 && interactionsManager.interaction.requiredTool == 2 && !individualInteractions.fishing && inventory.hasRod)
 					{
