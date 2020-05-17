5e9caf2222afeb91a12d0fe834bf1c8fb56f8ff0
Jamie <jmb1g17@soton.ac.uk>
2020-05-17 12:46:46 +0100

    Added geese to river

---
+++ Assets/Goose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goose : MonoBehaviour
+{
+    public GameObject[] geese;
+
+   
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Canoe")
+        {
+            for(int i =0; i < geese.Length; i++)
+            {   
+                geese[i].SetActive(true);
+            }   
+        }
+    }
+
+}
