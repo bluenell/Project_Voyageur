2c349ec390a2acfcb89bf176c6de72dda37f1ae4
Jamie <jmb1g17@soton.ac.uk>
2020-05-22 12:54:16 +0100

    added random fish in water

---
+++ Assets/AmbientFish.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientFish : MonoBehaviour
+{
+
+	Animator anim;
+	float timer;
+	bool generated;
+	int random;
+	int fish;
+
+	[SerializeField] Vector2 spawnRate;
+
+
+	private void Awake()
+	{
+		anim = transform.GetChild(0).GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+
+		if (!generated)
+		{
+			fish = Random.Range(0,2);
+			random = Mathf.FloorToInt(Random.Range(spawnRate.x, spawnRate.y));
+			generated = true;
+		}
+
+		if (timer > random)
+		{
+			if (fish == 0)
+			{
+				anim.SetTrigger("play");
+			}
+			else
+			{
+				anim.SetTrigger("play2");
+			}
+
+			timer = 0;
+			fish = 0;
+		}
+
+	}
+	
+
+}
